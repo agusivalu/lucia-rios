473f60db01786d396e9fe76caf0370f5e011db51
Lucia Rios <92958604+agusivalu@users.noreply.github.com>
2022-02-24 19:31:17 -0300

    Add files via upload

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ class Persona(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=60)
     apellido = models.CharField(max_length=100)
-    dni = models.CharField(max_length=50,blank=True)
-    signo = models.CharField(max_length=50)
+    nacimiento = models.CharField(max_length=50,blank=True)
+    edad = models.CharField(max_length=50)
     foto = models.ImageField(
         upload_to = 'foto/%Y/%m/%d',
         blank = True,
