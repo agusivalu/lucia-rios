e2217ff56f7c1a3fbe7c133779d03b79466edf13
Lucia Rios <92958604+agusivalu@users.noreply.github.com>
2022-02-24 21:14:51 -0300

    Update models.py

--- models.py
+++ models.py
@@ -8,8 +8,7 @@ class Persona(models.Model):
     apellido = models.CharField(max_length=100)
     nacimiento = models.CharField(max_length=50,blank=True)
     edad = models.CharField(max_length=50)
-	 dni=models.CharField(max_length=8)
-    fecha=models.DateField("2002","7","2")
+    dni=models.CharField(max_length=8)
     fecha=models.DateField(auto_now=True)
     club = models.CharField(max_length=50)
     foto = models.ImageField(
